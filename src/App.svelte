d53914fa18517a98eba0e78ec245b9c9ad49067e
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-07-07 19:51:54 -0700

    feat: create test application

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import PineappleCounter from "$lib/components/pineapple.svelte";
+  // import GalleryVerticalEndIcon from "@lucide/svelte/icons/gallery-vertical-end";
+</script>
+
+<div
+  class="dark bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+>
+  <div class="flex w-full max-w-sm flex-col gap-6">
+    <!-- <div
+      class="flex items-center gap-2 self-center font-medium text-foreground"
+    >
+      <div
+        class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+      >
+        <GalleryVerticalEndIcon class="size-4" />
+      </div>
+      Pineapples
+    </div> -->
+    <PineappleCounter />
+  </div>
+</div>
---
+++ src/lib/components/pineapple.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import ArrowUpIcon from "@lucide/svelte/icons/arrow-up";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import { fly } from "svelte/transition";
+
+  let pineapple = $state(0);
+</script>
+
+<Card.Root class="my-4 w-full max-w-sm">
+  <Card.Header>
+    <Card.Title>How Many Pineapples?</Card.Title>
+  </Card.Header>
+  <Card.Content>
+    {#key pineapple}
+      <p class="text-4xl font-bold" in:fly={{ y: -10, duration: 200 }}>
+        {pineapple}
+      </p>
+    {/key}
+    <p class="text-sm text-muted-foreground">
+      Pineapple
+    </p>
+    <br/>
+    <Button onclick={() => pineapple++} class="w-full">
+      Add Another <ArrowUpIcon />
+    </Button>
+  </Card.Content>
+</Card.Root>
